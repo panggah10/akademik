a9fe727ee75144cfb0e4058fc88a649da5950a30
Panggah Widiandana <panggah.widiandana10@gmail.com>
2024-06-04 13:04:33 +0700

    upload pertama

---
+++ api/index8.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INI TITLE</title>
+</head>
+
+<body>
+    <h1>HALO MUDA MULIA</h1>
+    <?php
+    $a = "Hallo Guys";
+
+    echo $a;
+    ?>
+
+</body>
+
+</html>
